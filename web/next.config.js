e0c925a8584d2aa8e6a6345a86c0b69b85c91896
noiseflood <cooljerk@noiseflood.com>
2025-10-28 04:26:08 -0500

    fix: add transpilePackages to Next.js config for workspace dependencies

--- web/next.config.js
+++ web/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     // Already set if you want to ignore TS errors too
     // ignoreBuildErrors: true,
   },
+  transpilePackages: ['@spa-audio/core', '@spa-audio/types', '@spa-audio/react'],
 };
 
 module.exports = nextConfig;
